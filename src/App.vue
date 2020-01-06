7dfb8272d84d0f6dfb3baf164718da72e36f2d26
Furqan Freed <furqanfreed@gmail.com>
2020-01-06 12:49:45 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <keep-alive>
+      <component v-bind:is="component" />
+    </keep-alive>
+
+    <button v-on:click="toggle">Toggle</button>
+  </div>
+</template>
+
+<script>
+
+import Component1 from './components/Component1.vue'
+import Component2 from './components/Component2.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Component1,
+     Component2
+  },
+  data (){
+    return {
+      component:"Component2"
+    }
+  },
+  methods: {
+    toggle(){
+      if (this.component === Component1) {
+        this.component = Component2;
+      } else {
+        this.component = Component1;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Component1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div><h1>I am Component 1</h1>
+       <form>
+         First name:<br>
+         <input type="text" name="firstname"><br>
+         Last name:<br>
+         <input type="text" name="lastname">
+       </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component1',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
